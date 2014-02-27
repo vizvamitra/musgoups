445f20cbf02588c32e87217b4078c2f42c52bd9e
vizvamitra <vizvamitra@gmail.com>
2014-02-27 16:59:29 +0400

    preparing to deploy...

--- config/deploy.rb
+++ config/deploy.rb
@@ -34,7 +34,7 @@ ssh_options[:forward_agent] = true
 # miscellaneous options
 set :deploy_via, :remote_cache
 set :scm, 'git'
-set :branch, 'design'
+set :branch, 'master'
 set :scm_verbose, true
 set :use_sudo, false
 set :normalize_asset_timestamps, false
